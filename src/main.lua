daf9d5a9c374a0e3ce4eeeb782627761ba72a09f
Robert Curtin <robertecurtin@gmail.com>
2018-02-10 19:48:28 -0500

    Doc cleanup and made example better

--- src/main.lua
+++ src/main.lua
@@ -6,10 +6,15 @@ local image_config = {
   count_x = 40,
   count_y = 30
 }
-local board = Board(image_config, 500)
+
+local board = Board(image_config, 10)
 local x = 1
 local y = 1
 board.run(function()
-  board.display_image(x,y,'../images/x.png')
+  board.display_image(x, y, '../images/x.png')
   x = x + 1
+  if x > image_config.count_x then
+    x = 1
+    y = y + 1
+  end
 end)
